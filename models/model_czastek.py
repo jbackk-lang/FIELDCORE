5e59fae5ebdb16dbc899a2ffe9851d1cb8446ad0
jbackk-lang <jbackk@gmail.com>
2026-06-08 17:03:25 +0200

    Add Czastka class with properties and description method

--- models/model_czastek.py
+++ models/model_czastek.py
@@ -1,1 +1,13 @@
 
+class Czastka:
+    def __init__(self, masa, ladunek, spin):
+        self.masa = masa
+        self.ladunek = ladunek
+        self.spin = spin
+
+    def opis(self):
+        return {
+            "masa": self.masa,
+            "ladunek": self.ladunek,
+            "spin": self.spin
+        }

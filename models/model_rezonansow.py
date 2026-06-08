3e516bb8990d65ecf74a07d9d61f839eaa002eec
jbackk-lang <jbackk@gmail.com>
2026-06-08 17:03:04 +0200

    Add Rezonans class with frequency and amplitude

--- models/model_rezonansow.py
+++ models/model_rezonansow.py
@@ -1,2 +1,12 @@
 # Model rezonansów – wersja wstępna
 # Tutaj będą funkcje opisujące powstawanie i zanikanie rezonansów
+class Rezonans:
+    def __init__(self, czestotliwosc, amplituda):
+        self.czestotliwosc = czestotliwosc
+        self.amplituda = amplituda
+
+    def opis(self):
+        return {
+            "f": self.czestotliwosc,
+            "A": self.amplituda
+        }

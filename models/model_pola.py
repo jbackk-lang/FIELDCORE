848775c8cfef5c67c51319f28fe595811f27d4a7
jbackk-lang <jbackk@gmail.com>
2026-06-08 17:02:43 +0200

    Update model_pola.py

--- models/model_pola.py
+++ models/model_pola.py
@@ -1,2 +1,12 @@
 # Model matematyczny pola – wersja wstępna
 # Tutaj trafią równania opisujące podstawową dynamikę pola
+class ModelPola:
+    def __init__(self, intensywnosc=1.0, harmoniczne=None):
+        self.intensywnosc = intensywnosc
+        self.harmoniczne = harmoniczne or []
+
+    def opis(self):
+        return {
+            "intensywnosc": self.intensywnosc,
+            "harmoniczne": self.harmoniczne
+        }

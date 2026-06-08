8221f4b4039deb36b4c3b7c7850a7ccbd2efa446
jbackk-lang <jbackk@gmail.com>
2026-06-08 17:50:36 +0200

    Add logging control methods to GlobalConfig

--- config/global_config.py
+++ config/global_config.py
@@ -1,4 +1,12 @@
 class GlobalConfig:
+    def wlacz_logi(self):
+        self.debug = True
+        return self
+
+    def wylacz_logi(self):
+        self.debug = False
+        return self
+
     def __init__(self):
         self.debug = False
         self.tryb_symulacji = "standard"

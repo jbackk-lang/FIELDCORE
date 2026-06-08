03054b4e95129c5e61ac6fbb25043b3eb9a7b5c1
jbackk-lang <jbackk@gmail.com>
2026-06-08 17:16:13 +0200

    Add TorusMobius class with initialization and description

---
+++ models/model_torus_mobius.py
@@ -0,0 +1,10 @@
+class TorusMobius:
+    def __init__(self, promien, skret):
+        self.promien = promien
+        self.skret = skret
+
+    def opis(self):
+        return {
+            "promien": self.promien,
+            "skret": self.skret
+        }
